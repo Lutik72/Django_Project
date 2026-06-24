10e6d7e257ccb7710fc80706260566ed081cdecd
Sofia <gribova.sofia24@yandex.ru>
2026-06-24 23:29:40 +0300

    обновление скрипта быстрого запуска

--- coffee_area/run.sh
+++ coffee_area/run.sh
@@ -1,6 +1,9 @@
 python -m venv venv
 . venv/Scripts/activate
 pip install -r requirements.txt
+touch apps/menu/migrations/__init__.py
+touch apps/coworking/migrations/__init__.py
+touch apps/booking/migrations/__init__.py
 python manage.py migrate
 touch apps/users/tests/__init__.py
 python manage.py test apps.users -v2
